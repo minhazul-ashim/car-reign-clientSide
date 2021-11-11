ee87437511181e4ec0b9a0259c73d5132011b00f
Minhazul Ashim <ashim.ashim7470@gmail.com>
2021-11-11 11:51:27 +0600

    registration form blur and click events organized

--- src/components/Pages/Register/RegisterForm.js
+++ src/components/Pages/Register/RegisterForm.js
@@ -1,34 +1,53 @@
 import { Button, Container, Grid, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import loginImg from '../../../images/loginimage.jpg'
 
 const RegisterForm = () => {
+
+    const [form, setForm] = useState({});
+
+    const handleBlur = (e) => {
+
+        const field = e.target.name;
+        const value = e.target.value;
+
+        const previousField = { ...form };
+        previousField[field] = value;
+
+        setForm(previousField);
+    }
+
+    const handleSubmit = () => {
+
+        console.log(form)
+    }
+
     return (
         <Container>
             <Grid container sx={{ p: '5%' }}>
                 <Grid item xs={12} md={5} sx={{ display: 'flex', justifyContent: 'center' }}>
                     <Box sx={{ width: '100%' }}>
 
-                        <TextField id="standard-basic" label="Your Name" variant="filled" sx={{ width: '100%', my: '2%' }} />
+                        <TextField name='name' onBlur={handleBlur} label="Your Name" variant="filled" sx={{ width: '100%', my: '2%' }} />
 
-                        <TextField id="standard-basic" label="Email" variant="filled" sx={{ width: '100%' }} />
+                        <TextField name='email' onBlur={handleBlur} label="Email" variant="filled" sx={{ width: '100%' }} />
 
-                        <TextField id="standard-basic" label="Password" type='password' variant="filled" sx={{ width: '100%', my: '2%' }} />
+                        <TextField name='password' onBlur={handleBlur} label="Password" type='password' variant="filled" sx={{ width: '100%', my: '2%' }} />
 
                         <Typography variant='body2'>
                             Already Registered? Please <Link to='/login'>Login.</Link>.
                         </Typography>
 
-                        <Button variant='outlined' sx={{ mb: '3%', display: 'block' }}>
+                        <Button onClick={handleSubmit} variant='outlined' sx={{ mb: '3%', display: 'block' }}>
                             Submit
                         </Button>
 
                         <Button variant='contained' sx={{ color: 'white', background: 'maroon' }}>
                             Login with Google
                         </Button>
-                        
+
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={7}>
