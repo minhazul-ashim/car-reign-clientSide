603f1e958b5711b71736e8ddb6ff77b332c31c1e
Minhazul Ashim <ashim.ashim7470@gmail.com>
2021-11-13 20:31:57 +0600

    dynamic rating stars has been added

--- src/components/Pages/Home/ReviewSection/ReviewBox.js
+++ src/components/Pages/Home/ReviewSection/ReviewBox.js
@@ -1,13 +1,15 @@
 import { Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
+import ReactStars from "react-rating-stars-component";
+
 
 const ReviewBox = ({ review }) => {
 
-    const { client, message, company, profession } = review;
+    const { client, message, company, profession, rating } = review;
 
     return (
         <Grid item xs={12} md={4} sx={{ minheight: '100%' }}>
-            <Paper elevation={1} sx={{ p: '5%', height: '100%' }}>
+            <Paper elevation={1} sx={{ p: '5%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Typography variant='h6' sx={{ textAlign: 'center' }}>
                     {client}
                 </Typography>
@@ -17,6 +19,13 @@ const ReviewBox = ({ review }) => {
                 <Typography variant='subtitle2' sx={{ textAlign: 'center', m: '3% 0' }}>
                     {company}
                 </Typography>
+
+                <ReactStars
+                    value={rating}
+                    size={24}
+                    activeColor="#ffd700"
+                />
+
                 <Typography variant='body2' sx={{ textAlign: 'center' }}>
                     "{message}"
                 </Typography>
