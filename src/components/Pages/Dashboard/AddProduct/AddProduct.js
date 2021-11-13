d2ba6b4631e01010095002b9cc0a6652585bbdc7
Minhazul Ashim <ashim.ashim7470@gmail.com>
2021-11-13 20:05:53 +0600

    adding a product api has been created

--- src/components/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/components/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,13 +1,37 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Paper, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 
 
 const AddProduct = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => {
+    const [cars, setCars] = useState([]);
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const loadProducts = () => {
+
+        fetch('http://localhost:5000/cars')
+            .then(res => res.json())
+            .then(data => {
+                setCars(data)
+            })
+    }
+
+    const handleDeletion = (id) => {
+
+        fetch(`http://localhost:5000/cars/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => loadProducts())
+    }
+
+    useEffect(loadProducts, [])
+
+
+    const onSubmit = (data) => {
 
         fetch('http://localhost:5000/cars', {
             method: 'POST',
@@ -17,43 +41,73 @@ const AddProduct = () => {
             body: JSON.stringify(data)
         })
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                loadProducts()
+                reset()
+            })
     };
 
     return (
-        <Box>
-            <Typography variant='h4'>Add A Product</Typography>
+        <>
+            <Box>
+                <Typography variant='h4'>Add A Product</Typography>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
-                <input {...register("name")} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter Product Name' />
-                {errors.name && <span>This field is required</span>}
+                    <input {...register("name")} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter Product Name' />
+                    {errors.name && <span>This field is required</span>}
 
-                <input {...register("manufrac", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the manufracturer name' />
-                {errors.manufrac && <span>This field is required</span>}
+                    <input {...register("manufrac", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the manufracturer name' />
+                    {errors.manufrac && <span>This field is required</span>}
 
-                <input {...register("engine", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the engine type' />
-                {errors.engine && <span>This field is required</span>}
+                    <input {...register("engine", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the engine type' />
+                    {errors.engine && <span>This field is required</span>}
 
-                <input {...register("weight", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter approximate weight' />
-                {errors.weight && <span>This field is required</span>}
+                    <input {...register("weight", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter approximate weight' />
+                    {errors.weight && <span>This field is required</span>}
 
-                <input {...register("category", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the vehicle category' />
-                {errors.category && <span>This field is required</span>}
+                    <input {...register("category", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the vehicle category' />
+                    {errors.category && <span>This field is required</span>}
 
-                <input {...register("price", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the selling price' />
-                {errors.price && <span>This field is required</span>}
+                    <input {...register("price", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the selling price' />
+                    {errors.price && <span>This field is required</span>}
 
-                <input {...register("img", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the direct URL to the vehicle image' />
-                {errors.img && <span>This field is required</span>}
+                    <input {...register("img", { required: true })} style={{ width: '75%', border: '1px solid #999', height: '25px', marginBottom: '2%' }} placeholder='Enter the direct URL to the vehicle image' />
+                    {errors.img && <span>This field is required</span>}
 
 
-                <Button type='submit' variant='contained' sx={{ display: 'block' }}>
-                    Submit
-                </Button>
-            </form>
+                    <Button type='submit' variant='contained' sx={{ display: 'block' }}>
+                        Submit
+                    </Button>
+                </form>
 
-        </Box>
+            </Box>
+
+            <Box width={1} marginTop='5%'>
+
+                <Typography variant='h4' sx={{ mb: '5%' }}>
+                    Current Available Products
+                </Typography>
+
+                {
+                    cars.map(car => {
+                        return <Paper elevation={0} sx={{ display: 'flex', justifyContent: 'space-between', mb: '2%' }}>
+
+                            <Typography variant='h6'>
+                                {car.name} ({car.manufrac})
+                            </Typography>
+
+                            <Button onClick={() => handleDeletion(car._id)} variant='contained' sx={{ background: 'rgba(255,0,0, 0.6)' }}>
+                                Delete
+                            </Button>
+
+                        </Paper>
+                    })
+                }
+
+            </Box>
+        </>
+
     );
 };
 
